008d95db23cf8967693e1c59e9ebea77b2544100
ane <128217704+anesakunic@users.noreply.github.com>
2023-12-14 19:54:29 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  purge: ['*html'],
+  corePlugins: {
+
+    preflight: false,
+
+  },
+  
+  prefix: 'tw-',
+  content: [],
+  theme: {
+    extend: {},
+  },
+  
+  plugins: [],
+}
+
